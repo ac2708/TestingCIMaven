3c8e755a9f3c6e8e0e64c901f446d21dd25aa2d4
akshay.chimote <akshay.chimote@globallogic.com>
2017-01-05 15:45:49 +0530

    Adding Maven Project

---
+++ src/test/java/DemoOfMaven.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by akshay.chimote on 05-01-2017.
+ */
+
+public class DemoOfMaven {
+    @Test
+    public void testGmail()
+    {
+        WebDriver driver = null;
+        try{
+
+            System.setProperty("webdriver.chrome.driver", "C:\\Eclipse Automation\\chrome prerequisites\\chromedriver.exe");
+
+            driver = new ChromeDriver();
+
+            // And now use this to visit Google
+            driver.get("http://www.gmail.com");
+            WebElement element = driver.findElement(By.id("Email"));
+            element.sendKeys("connectwithme11@gmail.com");
+            driver.findElement(By.id("next")).click();
+        /*driver.findElement(By.id("Passwd")).sendKeys("Akshay11");
+        driver.findElement(By.id("signIn")).click();
+        driver.navigate().to("https://www.google.co.in/intl/en/options/");*/
+            driver.navigate().back();
+            Assert.assertTrue(true);
+            driver.quit();
+        }
+        catch(Exception e)
+        {
+            driver.quit();
+        }
+    }
+
+}
